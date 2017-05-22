7d83ca70b95a7c6d87a0cce3a3f6c2d7d48d52be
dlt009 <davidsinovercos@gmail.com>
2017-05-22 14:24:32 -0700

    build:prod empty

--- gulp/shared/build.js
+++ gulp/shared/build.js
@@ -23,18 +23,19 @@ gulp.task('ng-annotate', ['concat:js'], function () {
 gulp.task('minify:css', ['concat:css'], function() {
   return gulp.src('./dist/bundle.css')
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
 });
 
 /* Minify bundle.js */
 gulp.task('minify:js', ['ng-annotate'], function() {
   return gulp.src('./dist/bundle.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
 });
 
 /* Build the app without minification */
 gulp.task('build:dev', ['dist']);
 
 /* Build the app and minfy */
-gulp.task('build:prod', ['dist', 'minify:js', /*'minify:css',*/ 'htmlify']);
+//gulp.task('build:prod', ['dist', 'minify:js', /*'minify:css',*/ 'htmlify']);
+gulp.task('build:prod');
