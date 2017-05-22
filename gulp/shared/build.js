388ba9d7acf8cfa2e39963d9e6b7d1e1d9db7924
dlt009 <davidsinovercos@gmail.com>
2017-05-22 14:31:12 -0700

    comments in yml

--- gulp/shared/build.js
+++ gulp/shared/build.js
@@ -37,5 +37,5 @@ gulp.task('minify:js', ['ng-annotate'], function() {
 gulp.task('build:dev', ['dist']);
 
 /* Build the app and minfy */
-//gulp.task('build:prod', ['dist', 'minify:js', /*'minify:css',*/ 'htmlify']);
-gulp.task('build:prod');
+gulp.task('build:prod', ['dist', 'minify:js', /*'minify:css',*/ 'htmlify']);
+
